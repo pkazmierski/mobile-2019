3fc00d1d1d993679d48f1d6e3d57d7092c818cba
Piotr Kocik <lifesim11@gmail.com>
2019-10-17 10:07:38 +0200

    WIP To-Do List implementation

--- app/src/main/java/com/example/kandydatpl/MainActivity.java
+++ app/src/main/java/com/example/kandydatpl/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.kandydatpl;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openChecklist(View view) {
+        Intent intent = new Intent(this, TaskListActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/kandydatpl/TaskListActivity.java
@@ -0,0 +1,63 @@
+package com.example.kandydatpl;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class TaskListActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+
+    private EditText itemET;
+    private Button btn;
+    private ListView itemsList;
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+
+        itemET = findViewById(R.id.item_edit_text);
+        btn = findViewById(R.id.add_btn);
+        itemsList = findViewById(R.id.items_list);
+
+        items = FileHelper.readData(this);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        itemsList.setAdapter(adapter);
+
+        btn.setOnClickListener(this);
+        itemsList.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.add_btn:
+                String itemEntered = itemET.getText().toString();
+                adapter.add(itemEntered);
+                itemET.setText("");
+                FileHelper.writeData(items, this);
+                Toast.makeText(this, "Item Added!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        items.remove(position);
+        adapter.notifyDataSetChanged();
+        FileHelper.writeData(items, this);
+        Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show();
+    }
+}
