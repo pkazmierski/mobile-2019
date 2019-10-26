e3d641dd3486f50f9ca309cdd529e6df37a57a31
Piotr Kaźmierski <217847@edu.p.lodz.pl>
2019-10-26 13:36:00 +0200

    Progress on basic login and DB integration.

--- app/src/main/java/com/example/kandydatpl/MainActivity.java
+++ app/src/main/java/com/example/kandydatpl/MainActivity.java
@@ -19,8 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initializeAppSync();
-
-
     }
 
     private void initializeAppSync() {
