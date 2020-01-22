59454c2929f15b06bae74bd51175f8bddd838092
Piotr Kaźmierski <217847@edu.p.lodz.pl>
2020-01-22 23:23:22 +0100

    Merge 'Added navigation drawer to the calendar. Fixed launching of multiple Checklist activities in the calendar. Fixed calendar's padding.'

--- app/src/main/java/com/example/kandydatpl/activities/NavigationDrawerActivity.java
+++ app/src/main/java/com/example/kandydatpl/activities/NavigationDrawerActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -22,6 +23,7 @@ public class NavigationDrawerActivity extends AppCompatActivity implements Navig
     protected DrawerLayout drawer;
     protected NavigationView navigationView;
     protected Toolbar toolbar;
+    protected ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +37,8 @@ public class NavigationDrawerActivity extends AppCompatActivity implements Navig
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        actionBar = getSupportActionBar();
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
